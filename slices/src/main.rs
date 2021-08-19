bf8ccdb8842660ea8cbaf3f6cd8a1ad84399aee4
MeenSeek Kim <meenseek5929@naver.com>
2021-08-19 18:19:13 +0900

    [Chapter4] - slices

---
+++ slices/src/main.rs
@@ -0,0 +1,27 @@
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+    for(i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return i;
+        }
+    }
+    s.len()
+}
+
+fn main() {
+    // let mut s = String::from("hello world");
+
+    // let word = first_word(&s); // word는 5를 갖게 될 것입니다.
+    // println!("word {}", word);
+    // s.clear(); // 이 코드는 String을 비워서 ""로 만들게 됩니다.
+    // word는 여기서 여전히 5를 갖고 있지만, 5라는 값을 의미있게 쓸 수 있는 스트링은 이제 없습니다.
+    // word는 이제 완전 유효하지 않습니다!
+    let s = String::from("hello");
+
+    let a = &s[0..5];
+    let slice = &s[0..2];
+    let slice = &s[..2];
+    let slice = &s[..3];
+    let a = &s[0..4]; 
+    println!("{}, {}",slice, a)
+}
